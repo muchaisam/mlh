7726f4e5e87bd90844430cdb33abe16308a77d2f
muchaisam <samson.muchai@strathmore.edu>
2021-09-27 17:11:20 +0300

    AlarmReceiver

---
+++ app/src/main/java/com/example/mlh/model/AlarmReceiver.java
@@ -0,0 +1,35 @@
+package com.example.mlh.model;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.mlh.R;
+import com.example.mlh.screens.AccountActivity;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent){
+
+        Intent i = new Intent(context, AccountActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,0, i,0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "SecureLine")
+                .setSmallIcon(R.drawable.topup)
+                .setContentTitle("Get back in the app to complete this!")
+                .setAutoCancel(true)
+                .setContentText("Hurry up before it's too late")
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        notificationManagerCompat.notify(123, builder.build());
+    }
+}
