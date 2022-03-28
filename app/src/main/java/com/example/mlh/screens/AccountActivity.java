3c789390da3a427b344a8ee301d7405a41eaa5f9
muchaisam <samson.muchai@strathmore.edu>
2022-03-29 00:35:17 +0300

    user info + layout update

--- app/src/main/java/com/example/mlh/screens/AccountActivity.java
+++ app/src/main/java/com/example/mlh/screens/AccountActivity.java
@@ -1,6 +1,5 @@
 package com.example.mlh.screens;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlarmManager;
@@ -9,19 +8,24 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mlh.MainActivity;
 import com.example.mlh.R;
 import com.example.mlh.databinding.ActivityAccountBinding;
 import com.example.mlh.model.AlarmReceiver;
+import com.example.mlh.user.LoggingActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.timepicker.MaterialTimePicker;
 import com.google.android.material.timepicker.TimeFormat;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.Calendar;
 
@@ -32,12 +36,46 @@ public class AccountActivity extends AppCompatActivity {
     private Calendar calendar;
     private AlarmManager alarmManager;
     private PendingIntent pendingIntent;
+    private TextView thename, themail, phone;
+    String firstname, lastname, useremail, usermobilenumber;
+    SharedPreferences sharedPreferences;
+    public final String SHARED_PREFS = "shared_prefs";
+    public final String FIRST_NAME = "first_name";
+    public final String LAST_NAME = "last_name";
+    public final String USER_EMAIL = "user_email";
+    public final String USER_PHONE = "user_mobile";
+    Button logout;
+    FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        //initializing shared prefs
+        sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        //getting data and storing it
+        firstname = sharedPreferences.getString(FIRST_NAME, null);
+        lastname = sharedPreferences.getString(LAST_NAME, null);
+        useremail = sharedPreferences.getString(USER_EMAIL, null);
+        usermobilenumber = sharedPreferences.getString(USER_PHONE, null);
+
+        //setting user data
+        thename = findViewById(R.id.thename);
+        themail = findViewById(R.id.themail);
+        phone = findViewById(R.id.phone);
+
+        thename.setText(firstname + " " +lastname);
+        themail.setText(useremail);
+        phone.setText(usermobilenumber);
+
+        logout = findViewById(R.id.btnlog);
+        logout.setOnClickListener(view -> {
+            FancyToast.makeText(AccountActivity.this, "You have been logged out", FancyToast.SUCCESS, FancyToast.LENGTH_LONG, false).show();
+            logout();
+        });
 
         //channel
         createNotificationChannel();
@@ -45,60 +83,34 @@ public class AccountActivity extends AppCompatActivity {
         binding = ActivityAccountBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         //onclick listeners for the three buttons
-        binding.timevieew.setOnClickListener (new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
+        binding.timevieew.setOnClickListener (v -> showTimePicker());
 
-                showTimePicker();
-            }
-        });
+        binding.setreminder.setOnClickListener(v -> setAlarm());
 
-        binding.setreminder.setOnClickListener( new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-
-                setAlarm();
-            }
-        });
-
-        binding.cancelreminder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                cancelAlarm();
-            }
-        });
+        binding.cancelreminder.setOnClickListener(v -> cancelAlarm());
 
 
         //initialize and assigning the variable
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNavigationView =  findViewById(R.id.bottom_navigation);
         //set home selected
         bottomNavigationView.setSelectedItemId(R.id.accountt);
         //listeners
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.accountt:
-                        return true;
-                    case R.id.home:
-                        Toast.makeText(AccountActivity.this, "Home", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.transactions:
-                        Toast.makeText(AccountActivity.this, "Transactions", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent (getApplicationContext(), TransactionsActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.payment:
-                        Toast.makeText(AccountActivity.this, "Payment", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent (getApplicationContext(), PaymentActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                }
-                return false;
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.accountt:
+                    return true;
+                case R.id.home:
+                    Toast.makeText(AccountActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.payment:
+                    Toast.makeText(AccountActivity.this, "Payment", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent (getApplicationContext(), PaymentActivity.class));
+                    overridePendingTransition(0,0);
+                    return true;
             }
+            return false;
         });
 
     }
@@ -113,7 +125,7 @@ public class AccountActivity extends AppCompatActivity {
         }
 
         alarmManager.cancel(pendingIntent);
-        Toast.makeText(this, "Reminder cancelled successfully", Toast.LENGTH_SHORT).show();
+        FancyToast.makeText(this, "Reminder cancelled successfully", FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, false).show();
     }
 
     private void setAlarm() {
@@ -125,7 +137,7 @@ public class AccountActivity extends AppCompatActivity {
         alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
                 AlarmManager.INTERVAL_DAY, pendingIntent);
 
-        Toast.makeText(this, "Reminder set successfully", Toast.LENGTH_SHORT).show();
+        FancyToast.makeText(this, "Reminder set successfully", FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, false).show();
     }
 
     private void showTimePicker() {
@@ -139,24 +151,21 @@ public class AccountActivity extends AppCompatActivity {
 
         picker.show(getSupportFragmentManager(), "SecureLine");
 
-        picker.addOnPositiveButtonClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        picker.addOnPositiveButtonClickListener(v -> {
 
-                if (picker.getHour() > 12){
-                    binding.timevieew.setText(
-                            String.format("%02d",(picker.getHour()-12))+ ":" +String.format("%02d", picker.getMinute()) + "PM"
-                    );
-                }else {
-                    binding.timevieew.setText(picker.getHour() + ":" + picker.getMinute() + "AM");
-                }
-
-                calendar = Calendar.getInstance();
-                calendar.set(Calendar.HOUR_OF_DAY, picker.getHour());
-                calendar.set(Calendar.MINUTE, picker.getMinute());
-                calendar.set(Calendar.SECOND, 0);
-                calendar.set(Calendar.MILLISECOND, 0);
+            if (picker.getHour() > 12){
+                binding.timevieew.setText(
+                        String.format("%02d",(picker.getHour()-12))+ ":" +String.format("%02d", picker.getMinute()) + "PM"
+                );
+            }else {
+                binding.timevieew.setText(picker.getHour() + ":" + picker.getMinute() + "AM");
             }
+
+            calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, picker.getHour());
+            calendar.set(Calendar.MINUTE, picker.getMinute());
+            calendar.set(Calendar.SECOND, 0);
+            calendar.set(Calendar.MILLISECOND, 0);
         });
     }
 
@@ -173,5 +182,10 @@ public class AccountActivity extends AppCompatActivity {
             notificationManager.createNotificationChannel(channel);
 
         }
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(AccountActivity.this, LoggingActivity.class));
     }
 }
